63089e5413703a4224df789ed5f61712b72ff8f4
zhs@ciklum.com <zhs@ciklum.com>
2017-02-21 14:11:28 +0200

    fixed scripts

--- scripts/version.js
+++ scripts/version.js
@@ -2,9 +2,10 @@ const plist = require('plist');
 const semver = require('semver');
 const fs = require('fs');
 const path = require('path');
-const packageJson = require('./package.json');
-
-const IOS_PROJECT_DIR = path.join('ios', 'epension');
+const VERSION_JSON = 'version.json';
+const PROJECT_DIR = process.cwd();
+const packageJson = require(path.join(PROJECT_DIR, 'package.json'));
+const IOS_PROJECT_DIR = path.join('ios', 'npmversion');
 const PLIST_FILE = 'Info.plist';
 const PLIST_VERSION_STRING_PARAM = 'CFBundleShortVersionString';
 const PLIST_BUNDLE_VERSION_PARAM = 'CFBundleVersion';
@@ -15,7 +16,7 @@ function increaseVersion (version) {
   let parsedPlist;
 
   try {
-    parsedPlist = plist.parse(fs.readFileSync(path.join(IOS_PROJECT_DIR, PLIST_FILE), 'utf8'));
+    parsedPlist = plist.parse(fs.readFileSync(path.join( PROJECT_DIR, IOS_PROJECT_DIR, PLIST_FILE), 'utf8'));
   } catch (e) {
     throw new Error(e);
   }
@@ -24,10 +25,10 @@ function increaseVersion (version) {
     parsedPlist[PLIST_BUNDLE_VERSION_PARAM] = version;
     parsedPlist[PLIST_VERSION_STRING_PARAM] = version;
 
-    fs.writeFileSync(path.join(IOS_PROJECT_DIR, PLIST_FILE), plist.build(parsedPlist));
+    fs.writeFileSync(path.join(PROJECT_DIR, IOS_PROJECT_DIR, PLIST_FILE), plist.build(parsedPlist));
   }
 
-  fs.writeFileSync('./version.json', `{ "version": ${PACKAGE_JSON_VERSION_PARAM}}`);
+  fs.writeFileSync(path.join(PROJECT_DIR, VERSION_JSON), `{ "version": "${PACKAGE_JSON_VERSION_PARAM}"}`);
 }
 
 increaseVersion(PACKAGE_JSON_VERSION_PARAM);
